c8ee91e8d9796e73c271081975f04062422a98fe
xdsolraC <xds0lr4c@gmail.com>
2024-03-14 14:12:30 -0400

    JS: initial structure

---
+++ script.js
@@ -0,0 +1,13 @@
+const booksList = document.querySelector(".booksList")
+const dialog = document.querySelector("dialog")
+const form = document.querySelector("form")
+
+let myLibrary = [];
+
+function Book() {
+    // the constructor...
+}
+
+function addBookToLibrary() {
+    // do stuff here
+}
