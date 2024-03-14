63a267f5e531b734acb3c8ea42eea80412c63309
xdsolraC <xds0lr4c@gmail.com>
2024-03-14 14:38:14 -0400

    JS: add library functions

--- script.js
+++ script.js
@@ -4,12 +4,35 @@ const form = document.querySelector("form")
 
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
     // the constructor...
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
-    // do stuff here
+// Library functions
+
+// Function to save books in library
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+}
+
+// Function to save library in localStorage
+function saveLibrary() {
+    localStorage.setItem("library", JSON.stringify(myLibrary))
+}
+
+// Function to get library from localStorage
+function getLibrary() {
+    const libraryData = JSON.parse(localStorage.getItem("library"));
+    if (libraryData) {
+        myLibrary = libraryData.map(data => {
+            const book = new Book(data.title, data.author, data.pages, data.read);
+            return book;
+        })
+    }
 }
 
 // Dialog functions
